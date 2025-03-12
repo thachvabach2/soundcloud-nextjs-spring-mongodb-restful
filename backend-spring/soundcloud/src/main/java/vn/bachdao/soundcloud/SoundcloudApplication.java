3bdb51320cc82e704d2431232920ffd20d7f9232
Bach Dao <ngocbach12a1@gmail.com>
2025-03-12 11:07:49 +0700

    [BE] Init Project

---
+++ backend-spring/soundcloud/src/main/java/vn/bachdao/soundcloud/SoundcloudApplication.java
@@ -0,0 +1,13 @@
+package vn.bachdao.soundcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundcloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoundcloudApplication.class, args);
+	}
+
+}
