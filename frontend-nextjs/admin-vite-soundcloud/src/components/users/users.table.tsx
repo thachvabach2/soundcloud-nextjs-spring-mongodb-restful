0cf2eae4b8bd435bf75f4432b1bab66a90f7c694
Bach Dao <ngocbach12a1@gmail.com>
2025-03-17 19:22:20 +0700

    [FE Admin] Fetch List User

--- frontend-nextjs/admin-vite-soundcloud/src/components/users/users.table.tsx
+++ frontend-nextjs/admin-vite-soundcloud/src/components/users/users.table.tsx
@@ -1,47 +1,62 @@
 
+import { useEffect, useState } from 'react';
 import '../../styles/users.css'
 
+interface IUsers {
+    email: string;
+    name: string;
+    role: string;
+}
+
 const UsersTable = () => {
+    const [listUser, setListUser] = useState([]);
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    const getData = async () => {
+        const access_token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbkBnbWFpbC5jb20iLCJleHAiOjE3NTA4NDk1NDMsImF1dGgiOiJST0xFX1VTRVIiLCJpYXQiOjE3NDIyMDk1NDN9.jPO75P0zy05boARYB8Jb99y-K6pXaNyc1ym2bAA11eHJ02es3esg5ET3Bw8a-_obE3nrsinjQ0FhwVzBUV2WSg';
+
+        const res = await fetch('http://localhost:8080/api/v1/users',
+            {
+                headers: {
+                    'Content-Type': "application/json",
+                    'Authorization': `Bearer ${access_token}`
+                }
+            }
+        )
+
+        const d = await res.json();
+        setListUser(d.data);
+    }
+
+    console.log('>>>> check List user: ', listUser);
     return (
         <div>
             <h2>HTML Table</h2>
 
             <table>
-                <tr>
-                    <th>Company</th>
-                    <th>Contact</th>
-                    <th>Country</th>
-                </tr>
-                <tr>
-                    <td>Alfreds Futterkiste</td>
-                    <td>Maria Anders</td>
-                    <td>Germany</td>
-                </tr>
-                <tr>
-                    <td>Centro comercial Moctezuma</td>
-                    <td>Francisco Chang</td>
-                    <td>Mexico</td>
-                </tr>
-                <tr>
-                    <td>Ernst Handel</td>
-                    <td>Roland Mendel</td>
-                    <td>Austria</td>
-                </tr>
-                <tr>
-                    <td>Island Trading</td>
-                    <td>Helen Bennett</td>
-                    <td>UK</td>
-                </tr>
-                <tr>
-                    <td>Laughing Bacchus Winecellars</td>
-                    <td>Yoshi Tannamuri</td>
-                    <td>Canada</td>
-                </tr>
-                <tr>
-                    <td>Magazzini Alimentari Riuniti</td>
-                    <td>Giovanni Rovelli</td>
-                    <td>Italy</td>
-                </tr>
+                <thead>
+                    <tr>
+                        <th>Email</th>
+                        <th>Name</th>
+                        <th>Role</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        listUser?.map((item: IUsers, index) => {
+                            return (
+                                <tr key={index}>
+                                    <td>{item.email}</td>
+                                    <td>{item.name}</td>
+                                    <td>{item.name}</td>
+                                </tr>
+                            )
+                        })
+                    }
+                </tbody>
             </table>
         </div>
     )
