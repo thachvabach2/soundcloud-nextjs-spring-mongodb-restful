39d36d74b82773a08b0d9f5d9e479d3d64021db6
Bach Dao <ngocbach12a1@gmail.com>
2025-07-11 12:49:30 +0700

    [BE] api: Get comments by a track

--- backend-spring/soundcloud/src/main/java/vn/bachdao/soundcloud/service/CommentService.java
+++ backend-spring/soundcloud/src/main/java/vn/bachdao/soundcloud/service/CommentService.java
@@ -143,9 +143,6 @@ public class CommentService {
                         .and("createdBy").as("createdBy")
                         .and("updatedBy").as("updatedBy"),
 
-                // Sắp xếp
-                // Aggregation.sort(Sort.by(Sort.Direction.DESC, "createdAt")),
-
                 // Sử dụng facet để count và paginate trong 1 query
                 Aggregation.facet(
                         Aggregation.skip(pageable.getOffset()),
@@ -209,5 +206,93 @@ public class CommentService {
         if (!trackExists) {
             throw new IdInvalidException("Comment với id = " + commentId + " không tồn tại");
         }
+    }
+
+    public ResPaginationDTO getCommentsByATrack(Pageable pageable, String trackId) {
+        Aggregation aggregation = Aggregation.newAggregation(
+                // Lọc comments chưa bị xóa
+                Aggregation.match(Criteria.where("isDeleted").is(false).and("track").is(trackId)),
+
+                // Convert string IDs to ObjectId
+                Aggregation.addFields()
+                        .addField("userObjectId").withValue(ConvertOperators.ToObjectId.toObjectId("$user"))
+                        .addField("trackObjectId").withValue(ConvertOperators.ToObjectId.toObjectId("$track"))
+                        .build(),
+
+                // Lookup để join với users và tracks
+                Aggregation.lookup("users", "userObjectId", "_id", "user"),
+                Aggregation.lookup("tracks", "trackObjectId", "_id", "track"),
+
+                // Unwind arrays
+                Aggregation.unwind("user", true),
+                Aggregation.unwind("track", true),
+
+                // Project các fields cần thiết
+                Aggregation.project()
+                        .and("_id").as("id")
+                        .and("content").as("content")
+                        .and("moment").as("moment")
+
+                        .and("user._id").as("user._id")
+                        .and("user.name").as("user.name")
+                        .and("user.email").as("user.email")
+                        .and("user.role").as("user.role")
+                        .and("user.type").as("user.type")
+
+                        .and("track._id").as("track._id")
+                        .and("track.title").as("track.title")
+                        .and("track.description").as("track.description")
+                        .and("track.trackUrl").as("track.trackUrl")
+
+                        .and("isDeleted").as("isDeleted")
+                        .and("createdAt").as("createdAt")
+                        .and("updatedAt").as("updatedAt")
+                        .and("createdBy").as("createdBy")
+                        .and("updatedBy").as("updatedBy"),
+
+                // Sử dụng facet để count và paginate trong 1 query
+                Aggregation.facet(
+                        Aggregation.sort(pageable.getSort()),
+                        Aggregation.skip(pageable.getOffset()),
+                        Aggregation.limit(pageable.getPageSize())).as("data")
+                        .and(Aggregation.count().as("count")).as("total"));
+
+        AggregationResults<Document> results = mongoTemplate.aggregate(
+                aggregation, "comments", Document.class);
+
+        Document result = results.getUniqueMappedResult();
+
+        // Extract data và total count
+        List<ResCommentDTO> comments = new ArrayList<>();
+        long totalElements = 0;
+
+        if (result != null) {
+            List<Document> dataList = result.getList("data", Document.class);
+            if (dataList != null) {
+                comments = dataList.stream()
+                        .map(doc -> mongoTemplate.getConverter().read(ResCommentDTO.class, doc))
+                        .collect(Collectors.toList());
+            }
+
+            List<Document> totalList = result.getList("total", Document.class);
+            if (totalList != null && !totalList.isEmpty()) {
+                totalElements = totalList.get(0).getInteger("count", 0);
+            }
+        }
+
+        Page<ResCommentDTO> commentPage = new PageImpl<>(comments, pageable, totalElements);
+        ResPaginationDTO res = new ResPaginationDTO();
+        ResPaginationDTO.Meta meta = new ResPaginationDTO.Meta();
+        meta.setPageNumber(commentPage.getNumber() + 1);
+        meta.setPageSize(commentPage.getSize());
+        meta.setTotalPage(commentPage.getTotalPages());
+        meta.setTotalElement(commentPage.getTotalElements());
+
+        List<ResCommentDTO> resCommentDTOs = commentPage.getContent();
+
+        res.setResult(resCommentDTOs);
+        res.setMeta(meta);
+
+        return res;
     }
 }
--- backend-spring/soundcloud/src/main/java/vn/bachdao/soundcloud/web/rest/user/CommentResource.java
+++ backend-spring/soundcloud/src/main/java/vn/bachdao/soundcloud/web/rest/user/CommentResource.java
@@ -1,21 +1,27 @@
 package vn.bachdao.soundcloud.web.rest.user;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
 import vn.bachdao.soundcloud.domain.dto.request.comment.ReqCreateCommentDTO;
+import vn.bachdao.soundcloud.domain.dto.response.ResPaginationDTO;
 import vn.bachdao.soundcloud.domain.dto.response.comment.ResCommentDTO;
 import vn.bachdao.soundcloud.service.CommentService;
 import vn.bachdao.soundcloud.util.annotation.ApiMessage;
+import vn.bachdao.soundcloud.util.annotation.ObjectIdValidator;
 import vn.bachdao.soundcloud.web.rest.errors.IdInvalidException;
 import vn.bachdao.soundcloud.web.rest.errors.UserNotAuthenticatedException;
 
 @RestController
 @RequestMapping("/api/v1")
+@Validated
 public class CommentResource {
     private final CommentService commentService;
 
@@ -28,5 +34,12 @@ public class CommentResource {
     public ResponseEntity<ResCommentDTO> createACommentOnATrack(@Valid @RequestBody ReqCreateCommentDTO req)
             throws IdInvalidException, UserNotAuthenticatedException {
         return ResponseEntity.ok(this.commentService.createACommentOnATrack(req));
+    }
+
+    @PostMapping("/tracks/comments")
+    @ApiMessage("Get comments by a track")
+    public ResponseEntity<ResPaginationDTO> getCommentsByATrack(Pageable pageable,
+            @RequestParam(value = "trackId") @ObjectIdValidator(message = "trackId: lỗi format ObjectId") String trackId) {
+        return ResponseEntity.ok(this.commentService.getCommentsByATrack(pageable, trackId));
     }
 }
