363d32a1aa505e47fafa64686566294d796c4f2a
Bach Dao <ngocbach12a1@gmail.com>
2025-07-20 04:31:00 +0700

    [BE] build: docker

--- backend-spring/soundcloud/src/main/java/vn/bachdao/soundcloud/config/MongoConfig.java
+++ backend-spring/soundcloud/src/main/java/vn/bachdao/soundcloud/config/MongoConfig.java
@@ -1,5 +1,6 @@
 package vn.bachdao.soundcloud.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoOperations;
@@ -10,9 +11,12 @@ import com.mongodb.client.MongoClients;
 
 @Configuration
 public class MongoConfig {
+    @Value("${soundcloud.mongo-uri}")
+    private String mongoUri;
+
     @Bean
     public MongoClient mongoClient() {
-        return MongoClients.create("mongodb+srv://ngocbach12a1:VXL2QqwQcpRSIrzX@cluster0.qfttq.mongodb.net/");
+        return MongoClients.create(mongoUri);
     }
 
     @Bean
--- backend-spring/soundcloud/src/main/java/vn/bachdao/soundcloud/web/rest/HelloResource.java
+++ backend-spring/soundcloud/src/main/java/vn/bachdao/soundcloud/web/rest/HelloResource.java
@@ -1,12 +1,18 @@
 package vn.bachdao.soundcloud.web.rest;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloResource {
+    private static final Logger LOG = LoggerFactory.getLogger(HelloResource.class);
+
     @GetMapping("/")
     public String getHello() {
+        LOG.info("GET API HELLO SUCCESS");
+
         return "Hello World";
     }
 }
