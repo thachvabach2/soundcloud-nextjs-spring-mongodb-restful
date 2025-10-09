9f5895d8bb6376a5efbc881d1c19d7267a0ef272
Bach Dao <ngocbach12a1@gmail.com>
2025-10-10 02:14:42 +0700

    [be] security add permit all request to check websocket https

--- backend-spring/soundcloud/src/main/java/vn/bachdao/soundcloud/config/SecurityConfig.java
+++ backend-spring/soundcloud/src/main/java/vn/bachdao/soundcloud/config/SecurityConfig.java
@@ -45,7 +45,7 @@ public class SecurityConfig {
                         .requestMatchers(AUTH_WHITELIST).permitAll()
                         .requestMatchers("/notification/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/v1/users").permitAll()
-                        .anyRequest().authenticated())
+                        .anyRequest().permitAll())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults())
                         .authenticationEntryPoint(customAuthenticationEntryPoint))
 
